fcd8ed037d46610bedc64f405d360018c34130b7
JoseCarlos <josee.cornejo@gmail.com>
2023-08-20 03:17:26 -0400

    cambios en la tabala de los tikect, cambios en editat ticket, error de carga de datos solucionado

--- ticketera/app/views.py
+++ ticketera/app/views.py
@@ -42,21 +42,11 @@ def editar_ticket(request, ticket_id):
     ticket = get_object_or_404(Ticket, pk=ticket_id)
 
     if request.method == 'POST':
-        form = TicketForm(request.POST)
+        form = TicketForm(request.POST, instance=ticket)  # Pasa la instancia al formulario
         if form.is_valid():
-            # Actualizar los campos del ticket con los datos del formulario
-            ticket.Sede = form.cleaned_data['Sede']
-            ticket.Direccion = form.cleaned_data['Direccion']
-            # Agrega más campos aquí
-
-            ticket.save()
+            form.save()  # Guarda los cambios en el ticket
             return redirect('listar_tickets')  # Cambia 'listar_tickets' por la URL correcta
     else:
-        # Inicializar el formulario con los datos del ticket
-        form = TicketForm(initial={
-            'Sede': ticket.Sede,
-            'Direccion': ticket.Direccion,
-            # Agrega más campos aquí
-        })
+        form = TicketForm(instance=ticket)  # Pasa la instancia al formulario
 
     return render(request, 'app/editar_ticket.html', {'form': form, 'ticket': ticket})
